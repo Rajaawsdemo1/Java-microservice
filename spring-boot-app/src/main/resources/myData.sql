19539f6869a3d391cbb6fab7a592f723f5bebaf2
shekaryadav <rajashekary19@gmail.com>
2019-10-03 23:02:33 -0400

    Added

---
+++ spring-boot-app/src/main/resources/myData.sql
@@ -0,0 +1,4 @@
+insert into employee(name) values ('saurabh');
+insert into employee(name) values ('devopssaurabh16@gmail.com');
+insert into employee(name) values ('AWS');
+insert into employee(name) values ('AWS-DevOps');
